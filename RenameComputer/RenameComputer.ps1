2e402f064e5839946bb9e69d7561ce6a2adfdae4
Jim Parris <jim@parrisfamily.com>
2024-03-03 07:59:04 -0600

    Cleanup

--- RenameComputer/RenameComputer.ps1
+++ RenameComputer/RenameComputer.ps1
@@ -1,5 +1,5 @@
-
-<#PSScriptInfo
+<#
+PSScriptInfo
 
 .VERSION 1.1
 
@@ -30,10 +30,6 @@ Version 1.0: Initial version.
 
 .PRIVATEDATA
 
-#>
-
-<# 
-
 .DESCRIPTION 
 Rename the computer using the logic contained in this script.  This will work for AD or AAD-joined devices, or even workgroup machines.
 Note that if the device is joined to AD, there must be connectivity to the domain and the computer must have the rights to rename itself.
@@ -42,28 +38,23 @@ Note that if the device is joined to AD, there must be connectivity to the domai
 
 Param()
 
-
 # If we are running as a 32-bit process on an x64 system, re-launch as a 64-bit process
-if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64")
-{
-    if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe")
-    {
+if ("$env:PROCESSOR_ARCHITEW6432" -ne 'ARM64') {
+    if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe") {
         & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy bypass -File "$PSCommandPath"
         Exit $lastexitcode
     }
 }
 
 # Create a tag file just so Intune knows this was installed
-if (-not (Test-Path "$($env:ProgramData)\Microsoft\RenameComputer"))
-{
-    Mkdir "$($env:ProgramData)\Microsoft\RenameComputer"
+if (-not (Test-Path "$($env:ProgramData)\Microsoft\RenameComputer")) {
+    mkdir "$($env:ProgramData)\Microsoft\RenameComputer"
 }
-Set-Content -Path "$($env:ProgramData)\Microsoft\RenameComputer\RenameComputer.ps1.tag" -Value "Installed"
+Set-Content -Path "$($env:ProgramData)\Microsoft\RenameComputer\RenameComputer.ps1.tag" -Value 'Installed'
 
 # Initialization
 $dest = "$($env:ProgramData)\Microsoft\RenameComputer"
-if (-not (Test-Path $dest))
-{
+if (-not (Test-Path $dest)) {
     mkdir $dest
 }
 Start-Transcript "$dest\RenameComputer.log" -Append
@@ -76,47 +67,48 @@ if ($details.CsPartOfDomain) {
     Write-Host "Device is joined to AD domain: $($details.CsDomain)"
     $isAD = $true
     $goodToGo = $false
-} else {
+}
+else {
     $goodToGo = $true
-    $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
+    $subKey = Get-Item 'HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo'
     $guids = $subKey.GetSubKeyNames()
-    foreach($guid in $guids) {
-        $guidSubKey = $subKey.OpenSubKey($guid);
-        $tenantId = $guidSubKey.GetValue("TenantId");
+    foreach ($guid in $guids) {
+        $guidSubKey = $subKey.OpenSubKey($guid)
+        $tenantId = $guidSubKey.GetValue('TenantId')
     }
     if ($null -ne $tenantID) {
         Write-Host "Device is joined to AAD tenant: $tenantID"
         $isAAD = $true
-    } else {
-        Write-Host "Not part of a AAD or AD, in a workgroup."
+    }
+    else {
+        Write-Host 'Not part of a AAD or AD, in a workgroup.'
     }
 }
 
 # Make sure we have connectivity
 $goodToGo = $true
 if ($isAD) {
-    $dcInfo = [ADSI]"LDAP://RootDSE"
-    if ($null -eq $dcInfo.dnsHostName)
-    {
-        Write-Host "No connectivity to the domain, unable to rename at this point."
+    $dcInfo = [ADSI]'LDAP://RootDSE'
+    if ($null -eq $dcInfo.dnsHostName) {
+        Write-Host 'No connectivity to the domain, unable to rename at this point.'
         $goodToGo = $false
     }
 }
 
 # Good to go, we can rename the computer
-if ($goodToGo)
-{
+if ($goodToGo) {
     # Remove the scheduled task (if it exists)
-    Disable-ScheduledTask -TaskName "RenameComputer" -ErrorAction Ignore
-    Unregister-ScheduledTask -TaskName "RenameComputer" -Confirm:$false -ErrorAction Ignore
-    Write-Host "Scheduled task unregistered."
+    Disable-ScheduledTask -TaskName 'RenameComputer' -ErrorAction Ignore
+    Unregister-ScheduledTask -TaskName 'RenameComputer' -Confirm:$false -ErrorAction Ignore
+    Write-Host 'Scheduled task unregistered.'
 
     # Get the new computer name: use the asset tag (maximum of 13 characters), or the 
     # serial number if no asset tag is available (replace this logic if you want)
     $systemEnclosure = Get-CimInstance -ClassName Win32_SystemEnclosure
-    if (($null -eq $systemEnclosure.SMBIOSAssetTag) -or ($systemEnclosure.SMBIOSAssetTag -eq "")) {
+    if (($null -eq $systemEnclosure.SMBIOSAssetTag) -or ($systemEnclosure.SMBIOSAssetTag -eq '')) {
         $assetTag = $details.BiosSerialNumber
-    } else {
+    }
+    else {
         $assetTag = $systemEnclosure.SMBIOSAssetTag
     }
     if ($assetTag.Length -gt 13) {
@@ -124,7 +116,8 @@ if ($goodToGo)
     }
     if ($details.CsPCSystemTypeEx -eq 1) {
         $newName = "D-$assetTag"
-    } else {
+    }
+    else {
         $newName = "L-$assetTag"
     }
 
@@ -140,49 +133,45 @@ if ($goodToGo)
     Rename-Computer -NewName $newName
 
     # Make sure we reboot if still in ESP/OOBE by reporting a 1641 return code (hard reboot)
-    if ($details.CsUserName -match "defaultUser")
-    {
-        Write-Host "Exiting during ESP/OOBE with return code 1641"
+    if ($details.CsUserName -match 'defaultUser') {
+        Write-Host 'Exiting during ESP/OOBE with return code 1641'
         Stop-Transcript
         Exit 1641
     }
     else {
-        Write-Host "Initiating a restart in 10 minutes"
-        & shutdown.exe /g /t 600 /f /c "Restarting the computer due to a computer name change.  Save your work."
+        Write-Host 'Initiating a restart in 10 minutes'
+        & shutdown.exe /g /t 600 /f /c 'Restarting the computer due to a computer name change.  Save your work.'
         Stop-Transcript
         Exit 0
     }
 }
-else
-{
+else {
     # Check to see if already scheduled
-    $existingTask = Get-ScheduledTask -TaskName "RenameComputer" -ErrorAction SilentlyContinue
-    if ($existingTask -ne $null)
-    {
-        Write-Host "Scheduled task already exists."
+    $existingTask = Get-ScheduledTask -TaskName 'RenameComputer' -ErrorAction SilentlyContinue
+    if ($null -ne $existingTask) {
+        Write-Host 'Scheduled task already exists.'
         Stop-Transcript
         Exit 0
     }
 
     # Copy myself to a safe place if not already there
-    if (-not (Test-Path "$dest\RenameComputer.ps1"))
-    {
+    if (-not (Test-Path "$dest\RenameComputer.ps1")) {
         Copy-Item $PSCommandPath "$dest\RenameComputer.PS1"
     }
 
     # Create the scheduled task action
-    $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -ExecutionPolicy bypass -WindowStyle Hidden -File $dest\RenameComputer.ps1"
+    $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-NoProfile -ExecutionPolicy bypass -WindowStyle Hidden -File $dest\RenameComputer.ps1"
 
     # Create the scheduled task trigger
-    $timespan = New-Timespan -minutes 5
+    $timespan = New-TimeSpan -Minutes 5
     $triggers = @()
     $triggers += New-ScheduledTaskTrigger -Daily -At 9am
     $triggers += New-ScheduledTaskTrigger -AtLogOn -RandomDelay $timespan
     $triggers += New-ScheduledTaskTrigger -AtStartup -RandomDelay $timespan
     
     # Register the scheduled task
-    Register-ScheduledTask -User SYSTEM -Action $action -Trigger $triggers -TaskName "RenameComputer" -Description "RenameComputer" -Force
-    Write-Host "Scheduled task created."
+    Register-ScheduledTask -User SYSTEM -Action $action -Trigger $triggers -TaskName 'RenameComputer' -Description 'RenameComputer' -Force
+    Write-Host 'Scheduled task created.'
 }
 
 Stop-Transcript
